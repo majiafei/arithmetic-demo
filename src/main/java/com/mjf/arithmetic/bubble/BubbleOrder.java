c15ec338973f57c4779a512f468d6d04cfb41754
hyson666666 <2854529108@qq.cpm>
2020-10-14 22:32:35 +0800

    冒泡算法 优化

--- src/main/java/com/mjf/arithmetic/bubble/BubbleOrder.java
+++ src/main/java/com/mjf/arithmetic/bubble/BubbleOrder.java
@@ -8,18 +8,24 @@ public class BubbleOrder {
 
     public static void bubbleOrder(int[] array) {
         for (int i = 1; i < array.length; i++) {
+            boolean hasOrder = true;
             for (int j = 0; j < array.length - i; j++) {
                 if (array[j] > array[j + 1]) {
                     int x = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = x;
+
+                    hasOrder = false;
                 }
+            }
+            if (hasOrder) {
+                break;
             }
         }
     }
 
     public static void main(String[] args) {
-        int[] array = {2, 1, 5, 3, -9};
+        int[] array = {2, 1, 5, 3, -9, 12};
         bubbleOrder(array);
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
