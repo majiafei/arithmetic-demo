00fb40a532c31e26d888c9b6cba4f23a2bee6274
hyson666666 <2854529108@qq.cpm>
2020-11-15 22:12:02 +0800

    冒泡排序算法优化：记录每一轮最后交换的位置，下一次只需要将这个位置之内的元素排序即可

--- src/main/java/com/mjf/arithmetic/bubble/BubbleOrder.java
+++ src/main/java/com/mjf/arithmetic/bubble/BubbleOrder.java
@@ -7,25 +7,31 @@ package com.mjf.arithmetic.bubble;
 public class BubbleOrder {
 
     public static void bubbleOrder(int[] array) {
+        // 最后一次交换的位置
+        int lastExchangePosition = 0;
+        int sortOrder = array.length - 1;
         for (int i = 1; i < array.length; i++) {
+            // 是否是有序的，没有交换过元素，说明是有序的
             boolean hasOrder = true;
-            for (int j = 0; j < array.length - i; j++) {
+            for (int j = 0; j < sortOrder; j++) {
                 if (array[j] > array[j + 1]) {
                     int x = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = x;
 
                     hasOrder = false;
+                    lastExchangePosition = j;
                 }
             }
             if (hasOrder) {
                 break;
             }
+            sortOrder = lastExchangePosition;
         }
     }
 
     public static void main(String[] args) {
-        int[] array = {2, 1, 5, 3, -9, 12};
+        int[] array = {2, 1, 5, 3, -9, 12, 13, 46};
         bubbleOrder(array);
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
